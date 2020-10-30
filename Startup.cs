99ca4842946273ea0ca9a8aa5a9eb2b991bce389
ktsik <Konstantinos.Tsikrikas@eurodyn.com>
2020-10-30 16:13:29 +0200

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,77 @@
+using AutoMapper;
+using MLRSIntroductoryWebApi.Data;
+using MLRSIntroductoryWebApi.MappingProfile;
+using MLRSIntroductoryWebApi.Models;
+using MLRSIntroductoryWebApi.Service;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Reflection;
+
+namespace MLRSIntroductoryWebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connectionString = Configuration["ConnectionStrings:UserContext"];
+            var serviceProvider = services.BuildServiceProvider();
+            var logger = serviceProvider.GetService<ILogger<User>>();
+
+            services.AddDbContext<UserContext>(o => o.UseSqlServer(connectionString));
+
+            services.AddAutoMapper(typeof(UserMappingProfile).GetTypeInfo().Assembly);
+            services.AddSingleton(typeof(ILogger), logger);
+            services.AddTransient<IUserRepository, UserRepository>();
+            services.AddTransient<IUserService, UserService>();
+            services.AddControllers().AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.PropertyNamingPolicy = null;
+                options.JsonSerializerOptions.DictionaryKeyPolicy = null;
+                options.JsonSerializerOptions.WriteIndented = true;
+            });
+
+            services.AddCors();
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddFile("Logs/log-{Date}.txt");
+
+            app.UseCors(options =>
+            options.WithOrigins("http://localhost:4200")
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
